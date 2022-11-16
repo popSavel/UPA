865efb396de294d02a0f0bfd741e33ba2d83d034
popSavel <dav.savel@gmail.com>
2022-11-16 13:38:22 +0100

    yeaboi

--- prvocisla.asm
+++ prvocisla.asm
@@ -1,36 +1,65 @@
 .data
 
      text:  .asciiz "Enter a number: "
+	 yes:	.asciiz "primary number"
+	 no:	.asciiz "not a primary number"
+	 printl: .asciiz "\n"
 
 .text
 
 main:
+	#vypis hlasky
     li $v0, 4
     la $a0, text
     syscall  
 	
+	#nacteni vstupu
 	li $v0, 5
     syscall
 	
-	# Moving the integer input to another register
+	# presun vstupu
 	move $t0, $v0
+	
+	#0 a 1 specialni pripad, nejsou prvocisla
+	beq	 $t0,0,nonprime
+	beq	 $t0,1,nonprime
+	
+	#promenne pro cyklus - od 2 do vstup-1
 	move $t1, $t0
 	addi $t1, $t1, -1
 	li $t2, 2
 	
-	li $v0, 1 #vypis
-    move $a0, $t1 #vypis
-	syscall
-	
 loop:
-    bgt $t2,$t1,exit
+    bgt $t2,$t1,prime
 	div $t0,$t2
+	#zbytek po deleni
 	mfhi $a2
-	move $a0, $a2
-    syscall
+	beq $a2,0,nonprime
 	addi $t2,$t2,1
     j loop 
+			
 	
+nonprime:
+	li $v0, 4
+	la $a0, no
+	syscall
+	
+	#odradkovani
+	li $v0, 4
+    la $a0, printl
+    syscall
+	j main
+
+prime:
+	li $v0, 4
+	la $a0, yes
+	syscall
+	
+	#odradkovani
+	li $v0, 4
+    la $a0, printl
+    syscall
+	j main
 	
 exit:	
 	jr $ra
